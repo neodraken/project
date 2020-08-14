400c607632824d9d10580df1bcf2a330d3a0d164
johanrc <johanrc.pro@gmail.com>
2020-08-14 11:51:09 +0200

    Ajout de react au site web(juste pour ajouter des fonctionnalités js à la page, React ne fera pas le routing) + exemple de la doc react button like

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ import React from "react";
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-// import '../css/app.css';
+import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
