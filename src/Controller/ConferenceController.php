149474441524c1c0b0a404af196c0be464d4b18b
johanrc <johanrc.pro@gmail.com>
2021-02-16 15:18:35 +0100

    templates twig display

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,41 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function index(string $name = ''): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
 
-        $greet = '';
-
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
+        // return new Response($twig->render('conference/index.html.twig', [
+        //     'conferences' => $conferenceRepository->findAll()
+        // ]));
 
 
-        return new Response(
-            <<<EOF
-            <html>
-            <body>
-               $greet
-                <img src="/images/under-construction.gif" />
-            </body>
-        </html>
-        EOF
-        );
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+        ]);
+    }
 
+    /**
+     * @Route("/conference/{id}", name="conference")
+     *
+     */
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
-
-        // return $this->render('conference/index.html.twig', [
-        //     'controller_name' => 'ConferenceController',
-        // ]);
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+        ]);
     }
 }
